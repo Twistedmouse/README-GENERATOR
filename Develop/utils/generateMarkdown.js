141ee444f662793282fc3007dfcb4b0435143b7e
tristan <mousy93@hotmail.com>
2021-05-24 13:11:44 +0800

    readme demo vid test

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -38,7 +38,7 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(answers) {
   if (answers.license === "Apache") {
@@ -195,6 +195,8 @@ ${plansConfirmed(answers.future)}
 ---
 ---
 ${renderLicenseSection(answers)}
+
+---
 
 `;
 }
